bbdabfa3cae8683a0e39c036fa683f17fc305b2b
Kurumiim <rukattsu@gmail.com>
2023-07-14 16:59:16 -0300

    atualizando desafio 8

--- src/Desafio8.java
+++ src/Desafio8.java
@@ -5,8 +5,12 @@ public class Desafio8 {
         Scanner scanner = new Scanner(System.in);
 
 
-        for (int ano = 1004; ano <= 2017; ano+=4){
+        for (int ano = 1004; ano <= 2024; ano+=4){
+            if (!(ano % 100 == 0) || (ano % 400 == 0)){
             System.out.println(ano + "\n");
+            }else{
+                System.out.println(ano + " Não é bissexto \n");
+            }
         }
         scanner.close();
     }
